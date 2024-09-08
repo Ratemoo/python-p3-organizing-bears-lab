0758b5caa1216209b4ac0ed68871651ec9487652
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-09-08 21:59:18 +0300

    >

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+    ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 3, 'F', 'Brown', 'Friendly', 1),
+    ('Sergeant Brown', 15, 'M', 'Brown', 'Aggressive', 0),
+    ('Melissa', 5, 'F', 'Black', 'Calm', 1),
+    ('Grinch', 4, 'M', 'White', 'Grumpy', 0),
+    ('Wendy', 2, 'F', 'Brown', 'Playful', 1),
+    (NULL, 1, 'M', 'Brown', 'Unknown', 1);
